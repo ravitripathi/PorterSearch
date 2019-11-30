2ca6219d58631a23ed6b69e11ddbe62efb72de4f
Ravi Tripathi <ravi.tripathi@gdn-commerce.com>
2019-11-30 22:55:29 +0530

    UI for Search box done

--- Porter-Assignment/HomeController.swift
+++ Porter-Assignment/HomeController.swift
@@ -10,35 +10,43 @@ import UIKit
 import MapKit
 
 class HomeController: UIViewController {
-  @IBOutlet weak var pickupButton: UIButton!
-  @IBOutlet weak var dropButton: UIButton!
-  
-  @IBOutlet weak var mapView: MKMapView!
-  @IBOutlet weak var bookButton: UIButton!
-  
-  /* Service */
-  private var porterService: PorterService = HttpPorterService()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    initViews()
-  }
-  
-  private func initViews() {
-    pickupButton.addTarget(self, action: #selector(self.didTapPickupLocation), for: .touchUpInside)
-    dropButton.addTarget(self, action: #selector(self.didTapDropLocation), for: .touchUpInside)
-    bookButton.addTarget(self, action: #selector(self.didTapBook), for: .touchUpInside)
-  }
-  
-  @objc private func didTapPickupLocation() {
-    performSegue(withIdentifier: Segues.SearchLocation.rawValue, sender: self)
-  }
-  
-  @objc private func didTapDropLocation() {
-    performSegue(withIdentifier: Segues.SearchLocation.rawValue, sender: self)
-  }
-  
-  @objc private func didTapBook() {
-
-  }
+    //  @IBOutlet weak var pickupButton: UIButton!
+    //  @IBOutlet weak var dropButton: UIButton!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    //  @IBOutlet weak var bookButton: UIButton!
+    /* Service */
+    private var porterService: PorterService = HttpPorterService()
+    
+    let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        return manager
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViews()
+    }
+    
+    private func initViews() {
+        self.mapView.showsUserLocation = true
+        let button = MKUserTrackingButton(mapView: self.mapView)
+        button.backgroundColor = UIColor.white
+        self.mapView.addSubview(button)
+        button.anchor(top: nil, left: nil, bottom: self.mapView.bottomAnchor, right: self.mapView.rightAnchor, paddingTop: 0.0, paddingLeft: 0.0, paddingBottom: 16.0, paddingRight: 16.0, width: 32.0, height: 32.0)
+    }
+    
+    @objc private func didTapPickupLocation() {
+        performSegue(withIdentifier: Segues.SearchLocation.rawValue, sender: self)
+    }
+    
+    @objc private func didTapDropLocation() {
+        performSegue(withIdentifier: Segues.SearchLocation.rawValue, sender: self)
+    }
+    
+    @objc private func didTapBook() {
+        
+    }
 }
--- Porter-Assignment/Utility.swift
+++ Porter-Assignment/Utility.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Shameek Sarkar. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import MapKit
 
 class Utility {
@@ -20,5 +20,68 @@ class Utility {
         let lng = Double(mapItem.placemark.coordinate.longitude)
         let location = Location(lat: lat, lng: lng)
         return Place(name: name, address: address, location: location)
+    }
+}
+
+extension UIView {
+    func anchor(top : NSLayoutYAxisAnchor?, left : NSLayoutXAxisAnchor?, bottom : NSLayoutYAxisAnchor?,right : NSLayoutXAxisAnchor?,paddingTop : CGFloat,paddingLeft : CGFloat,paddingBottom : CGFloat, paddingRight : CGFloat,width : CGFloat,height : CGFloat){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top{
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let left = left{
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let right = right{
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if(width != 0){
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if(height != 0){
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
+
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
     }
 }
