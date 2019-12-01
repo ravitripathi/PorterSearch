1326472eb09d227c6f4892e0d9932a7477cd23a7
Ravi Tripathi <ravi.tripathi@gdn-commerce.com>
2019-12-02 01:47:36 +0530

    Removed unused file

--- Porter-Assignment/Views/CustomAnnotationView.swift
+++ Porter-Assignment/Views/CustomAnnotationView.swift
@@ -15,7 +15,7 @@ protocol CustomAnnotationViewDelegate {
 
 class CustomAnnotationView: MKAnnotationView {
     
-     private let inset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    private let inset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
     
     @IBOutlet weak var etaLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
@@ -38,64 +38,34 @@ class CustomAnnotationView: MKAnnotationView {
         var point: CGPoint = CGPoint(x: bounds.size.width - inset.right, y: bounds.size.height - inset.bottom)
         var controlPoint: CGPoint
         path.move(to: point)
-        // lower right
         point = CGPoint(x: bounds.size.width / 2.0 + inset.bottom, y: bounds.size.height - inset.bottom)
         path.addLine(to: point)
-        
-        // right side of arrow
-        
         controlPoint = CGPoint(x: bounds.size.width / 2.0, y: bounds.size.height - inset.bottom)
         point = CGPoint(x: bounds.size.width / 2.0, y: bounds.size.height)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        
-        // left of pointer
         controlPoint = CGPoint(x: point.x, y: bounds.size.height - inset.bottom)
         point = CGPoint(x: point.x - inset.bottom, y: controlPoint.y)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        // bottom left
-        
         point.x = inset.left
         path.addLine(to: point)
-        
-        // lower left corner
-        
         controlPoint = CGPoint(x: 0, y: bounds.size.height - inset.bottom)
         point = CGPoint(x: 0, y: controlPoint.y - inset.left)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        
-        // left
-        
         point.y = inset.top
         path.addLine(to: point)
-        
-        // top left corner
-        
         controlPoint = CGPoint.zero
         point = CGPoint(x: inset.left, y: 0)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        
-        // top
-        
         point = CGPoint(x: bounds.size.width - inset.left, y: 0)
         path.addLine(to: point)
-        
-        // top right corner
-        
         controlPoint = CGPoint(x: bounds.size.width, y: 0)
         point = CGPoint(x: bounds.size.width, y: inset.top)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        
-        // right
-        
         point = CGPoint(x: bounds.size.width, y: bounds.size.height - inset.bottom - inset.right)
         path.addLine(to: point)
-        
-        // lower right corner
-        
         controlPoint = CGPoint(x: bounds.size.width, y: bounds.size.height - inset.bottom)
         point = CGPoint(x: bounds.size.width - inset.right, y: bounds.size.height - inset.bottom)
         path.addQuadCurve(to: point, controlPoint: controlPoint)
-        
         path.close()
         bubbleLayer.path = path.cgPath
         layer.insertSublayer(bubbleLayer, at: 0)
